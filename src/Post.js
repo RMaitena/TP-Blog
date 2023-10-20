06a7050a073a5718c94b735fa9c8331afeb5975e
Melu <melacoton11@gmail.com>
2023-10-20 18:27:57 -0300

    El comiteado

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-function Post (props) {
+import { Link } from "react-router-dom";
+
+export default function Post (props) {
 return (
 <div className='texto'>
 <h3>{props.titulo}</h3>
 <p>{props.descripcion}</p>
-<button className='coment'>Comentar</button>
+<Link to={props.link}>
 <h6 className='ver'>Ver comentarios</h6>
+</Link>
 </div>
 );
 }
-export default Post;
